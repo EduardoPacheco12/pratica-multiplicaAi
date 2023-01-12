709f376a1886867b884a9e1f084d370ca52d77f9
EduardoPacheco12 <eduardooliveirapacheco131@gmail.com>
2023-01-12 18:14:54 -0300

    código atualizado e melhor otimizado

--- src/App.java
+++ src/App.java
@@ -8,14 +8,9 @@ public class App {
         int valor = input.nextInt();
 
         System.out.println("Tabela de multiplicação de " + valor);
-        System.out.println(valor + " x 1 = " + (valor * 1));
-        System.out.println(valor + " x 2 = " + (valor * 2));
-        System.out.println(valor + " x 3 = " + (valor * 3));
-        System.out.println(valor + " x 4 = " + (valor * 4));
-        System.out.println(valor + " x 5 = " + (valor * 5));
-        System.out.println(valor + " x 6 = " + (valor * 6));
-        System.out.println(valor + " x 7 = " + (valor * 7));
-        System.out.println(valor + " x 8 = " + (valor * 8));
-        System.out.println(valor + " x 9 = " + (valor * 9));
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(valor + " x " + i + " = " + (valor * i));
+        }
+        
     }
 }
